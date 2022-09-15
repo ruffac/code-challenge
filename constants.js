5d52e97b1352212e84ac4b4be9781d3a7657056d
ruffa <ruffa@gmail>
2022-09-15 20:13:18 +0800

    feat: added katas

--- constants.js
+++ constants.js
@@ -1,4 +1,25 @@
-export const challengeIds = ["53b138b3b987275b46000115"];
+export const challengeIds = [
+  "59441520102eaa25260000bf",
+  "5b853229cfde412a470000d0",
+  "53b138b3b987275b46000115",
+  "56efc695740d30f963000557",
+  "52fba66badcd10859f00097e",
+  "5420fc9bb5b2c7fd57000004",
+  "54e6533c92449cc251001667",
+  "6319dba6d6e2160015a842ed",
+  "51c8991dee245d7ddf00000e",
+  "5727bb0fe81185ae62000ae3",
+  "515e188a311df01cba000003",
+  "515e188a311df01cba000003",
+  "57ee99a16c8df7b02d00045f",
+  "521c2db8ddc89b9b7a0000c1",
+  "5694cb0ec554589633000036",
+  "5703c093022cd1aae90012c9",
+  "55a144eff5124e546400005a",
+  "56b97b776ffcea598a0006f2",
+  "57fe864854685b1c420002e0",
+  "563d59dd8e47a5ed220000ba",
+];
 export const defaultChallengeDuration = 42; // in days
 export const defaultCodewarsChallenge =
   "https://www.codewars.com/collections/upliftcodecamp";
